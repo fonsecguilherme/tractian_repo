8930c367e0d2cacf6a48aa30d94f3e0f0e045330
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2024-06-25 20:28:34 -0300

    feat: add asset page store dependency

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tractian_application/presentation/asset_page/store/asset_page_store.dart';
 import 'package:tractian_application/presentation/home_page/home_page.dart';
 import 'package:tractian_application/presentation/home_page/store/home_page_store.dart';
 
@@ -15,6 +16,11 @@ class App extends StatelessWidget {
         Provider(create: (context) => CompaniesRepository()),
         ChangeNotifierProvider(
           create: (context) => HomePageStore(
+            repository: context.read<CompaniesRepository>(),
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => AssetPageStore(
             repository: context.read<CompaniesRepository>(),
           ),
         )
