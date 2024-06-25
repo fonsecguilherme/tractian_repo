110c7bb92dc88f405409cc34672d68a3befca898
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2024-06-25 20:29:59 -0300

    feat: created new http request get asset

--- lib/domain/companies_repository.dart
+++ lib/domain/companies_repository.dart
@@ -4,8 +4,11 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:tractian_application/data/models/company_model.dart';
 
+import '../data/models/asset_model.dart';
+
 abstract class ICompaniesRepository {
   Future<List<CompanyModel>> getCompanies();
+  Future<List<AssetModel>> getAssets({required String companyID});
 }
 
 class CompaniesRepository implements ICompaniesRepository {
@@ -26,6 +29,37 @@ class CompaniesRepository implements ICompaniesRepository {
               jsonData.map((company) => CompanyModel.fromMap(company)).toList();
 
           return companies;
+
+        case HttpStatus.badRequest:
+          throw Exception('Bad request: ${response.statusCode}');
+
+        case HttpStatus.notFound:
+          throw Exception('Failed to find the url. ${response.statusCode}');
+
+        default:
+          throw Exception('Failed to load companies: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  Future<List<AssetModel>> getAssets({required String companyID}) async {
+    final url = '$_baseUrl/companies/$companyID/assets';
+
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      switch (response.statusCode) {
+        case HttpStatus.ok:
+          List<dynamic> jsonData = jsonDecode(response.body);
+          List<AssetModel> assets =
+              jsonData.map((asset) => AssetModel.fromMap(asset)).toList();
+
+          print(assets);
+
+          return assets;
 
         case HttpStatus.badRequest:
           throw Exception('Bad request: ${response.statusCode}');
