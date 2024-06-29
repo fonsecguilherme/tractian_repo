c1c186df0d2d3aba6adb5b10aa04ee220153bead
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2024-06-28 23:28:40 -0300

    feat: new filter functions to store class

--- lib/presentation/asset_page/store/asset_page_store.dart
+++ lib/presentation/asset_page/store/asset_page_store.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tractian_application/data/models/asset_model.dart';
 import 'package:tractian_application/presentation/asset_page/asset_page_state.dart';
 
 import '../../../domain/companies_repository.dart';
@@ -21,4 +22,28 @@ class AssetPageStore extends ValueNotifier<AssetPageState> {
       value = ErrorAssetPageState(errorMessage: e.toString());
     }
   }
+
+
+    Map<String, List<AssetModel>> groupAssetsByParentID(List<AssetModel> assets) {
+    Map<String, List<AssetModel>> groupedAssets = {};
+    for (var asset in assets) {
+      if (groupedAssets[asset.parentId] == null) {
+        groupedAssets[asset.parentId ?? ''] = [];
+      }
+      groupedAssets[asset.parentId ?? '']!.add(asset);
+    }
+    return groupedAssets;
+  }
+
+   List<AssetModel> filterAssets(
+      List<AssetModel> assets, String query, bool sensor, bool isCritic) {
+    return assets.where((asset) {
+      bool matchesQuery =
+          asset.name.toLowerCase().contains(query.toLowerCase());
+      bool matchesSensorType = !sensor || asset.sensorType == 'energy';
+      bool matchesStatus = !isCritic || asset.status == 'alert';
+      return matchesQuery && matchesSensorType && matchesStatus;
+    }).toList();
+  }
+
 }
